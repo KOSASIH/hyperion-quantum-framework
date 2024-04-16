51193441416ae871362b61141b383d175941ca88
KOSASIH <kosasihg88@gmail.com>
2024-04-16 21:37:05 +0700

    Create measurement.py

---
+++ hyperion/test/measurement/measurement.py
@@ -0,0 +1,16 @@
+# hyperion/test/measurement.py
+
+def measure_brightness():
+    """Measure the brightness of a Hyperion instance."""
+    # Code to measure brightness goes here
+    pass
+
+def measure_color_temperature():
+    """Measure the color temperature of a Hyperion instance."""
+    # Code to measure color temperature goes here
+    pass
+
+def measure_saturation():
+    """Measure the saturation of a Hyperion instance."""
+    # Code to measure saturation goes here
+    pass
